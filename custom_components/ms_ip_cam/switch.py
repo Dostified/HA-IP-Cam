bab94a4b014d91c6e114611d54030131feacb6c4
Dostified <146396287+Dostified@users.noreply.github.com>
2026-08-13 15:51:48 +0530

    Update switch.py

--- custom_components/ms_ip_cam/switch.py
+++ custom_components/ms_ip_cam/switch.py
@@ -2,10 +2,11 @@ from homeassistant.components.switch import SwitchEntity
 import requests
 
 class FlashlightSwitch(SwitchEntity):
-    def __init__(self, ip_address: str):
+    def __init__(self, ip_address: str, http_port: int):
         self._attr_name = "MS IP Cam Flashlight"
         self._is_on = False
-        self._ip = ip_address # Saves the dynamic IP
+        self._ip = ip_address
+        self._port = http_port
 
     @property
     def is_on(self):
@@ -13,19 +14,19 @@ class FlashlightSwitch(SwitchEntity):
 
     def turn_on(self, **kwargs):
         try:
-            requests.get(f"http://{self._ip}:8080/flash?state=on", timeout=3)
+            requests.get(f"http://{self._ip}:{self._port}/flash?state=on", timeout=3)
             self._is_on = True
         except requests.exceptions.RequestException:
             pass
 
     def turn_off(self, **kwargs):
         try:
-            requests.get(f"http://{self._ip}:8080/flash?state=off", timeout=3)
+            requests.get(f"http://{self._ip}:{self._port}/flash?state=off", timeout=3)
             self._is_on = False
         except requests.exceptions.RequestException:
             pass
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    """Set up the switch from a config entry."""
     ip_address = entry.data["ip_address"]
-    async_add_entities([FlashlightSwitch(ip_address)])
+    http_port = entry.data.get("http_port", 8080)
+    async_add_entities([FlashlightSwitch(ip_address, http_port)])
