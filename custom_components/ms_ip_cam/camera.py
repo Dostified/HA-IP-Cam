22f6a64229844b5c55b5e739cbb7258d6cb68818
Dostified <146396287+Dostified@users.noreply.github.com>
2026-08-13 15:58:30 +0530

    Update camera.py

--- custom_components/ms_ip_cam/camera.py
+++ custom_components/ms_ip_cam/camera.py
@@ -1,10 +1,12 @@
 from homeassistant.components.camera import Camera, CameraEntityFeature
 
+DOMAIN = "ms_ip_cam"
+
 class MyProCamera(Camera):
-    def __init__(self, ip_address: str, rtsp_port: int):
+    def __init__(self, rtsp_url: str):
         super().__init__()
         self._attr_name = "MS IP Cam Feed"
-        self._stream_source = f"rtsp://{ip_address}:{rtsp_port}/live"
+        self._stream_source = rtsp_url
         self._attr_supported_features = CameraEntityFeature.STREAM
 
     @property
@@ -15,6 +17,5 @@ class MyProCamera(Camera):
         return self._stream_source
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    ip_address = entry.data["ip_address"]
-    rtsp_port = entry.data["rtsp_port"]
-    async_add_entities([MyProCamera(ip_address, rtsp_port)])
+    data = hass.data[DOMAIN][entry.entry_id]
+    async_add_entities([MyProCamera(data["rtsp_url"])])
