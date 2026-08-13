82b9f84d3336e29f437f695bd926a542c8aed0de
Dostified <146396287+Dostified@users.noreply.github.com>
2026-08-13 15:08:21 +0530

    Update __init__.py

--- custom_components/ms_ip_cam/__init__.py
+++ custom_components/ms_ip_cam/__init__.py
@@ -1,9 +1,18 @@
 from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 
 DOMAIN = "ms_ip_cam"
+PLATFORMS = ["camera", "switch", "select"]
 
-def setup(hass: HomeAssistant, config: dict) -> bool:
-    hass.helpers.discovery.load_platform("camera", DOMAIN, {}, config)
-    hass.helpers.discovery.load_platform("switch", DOMAIN, {}, config)
-    hass.helpers.discovery.load_platform("select", DOMAIN, {}, config)
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up MS IP Cam from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    # Store the user's IP address in HA's memory
+    hass.data[DOMAIN][entry.entry_id] = entry.data 
+    
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
