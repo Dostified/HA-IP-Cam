82b485ab2caa5d95d2a090aa72d253cfecac66d1
Dostified <146396287+Dostified@users.noreply.github.com>
2026-08-13 15:52:29 +0530

    Update switch.py

--- custom_components/ms_ip_cam/switch.py
+++ custom_components/ms_ip_cam/switch.py
@@ -1,21 +1,32 @@
-from homeassistant.components.button import ButtonEntity
+from homeassistant.components.switch import SwitchEntity
 import requests
 
-class CameraShutterButton(ButtonEntity):
+class FlashlightSwitch(SwitchEntity):
     def __init__(self, ip_address: str, http_port: int):
-        self._attr_name = "MS IP Cam Shutter Button"
-        self._attr_icon = "mdi:camera-iris"
+        self._attr_name = "MS IP Cam Flashlight"
+        self._is_on = False
         self._ip = ip_address
         self._port = http_port
 
-    def press((self) -> None:
-        """Handle the button press to trigger camera shutter."""
+    @property
+    def is_on(self):
+        return self._is_on
+
+    def turn_on(self, **kwargs):
         try:
-            requests.get(f"http://{self._ip}:{self._port}/shutter", timeout=3)
+            requests.get(f"http://{self._ip}:{self._port}/flash?state=on", timeout=3)
+            self._is_on = True
+        except requests.exceptions.RequestException:
+            pass
+
+    def turn_off(self, **kwargs):
+        try:
+            requests.get(f"http://{self._ip}:{self._port}/flash?state=off", timeout=3)
+            self._is_on = False
         except requests.exceptions.RequestException:
             pass
 
 async def async_setup_entry(hass, entry, async_add_entities):
     ip_address = entry.data["ip_address"]
     http_port = entry.data.get("http_port", 8080)
-    async_add_entities([CameraShutterButton(ip_address, http_port)])
+    async_add_entities([FlashlightSwitch(ip_address, http_port)])
