7fe5117c2fd03c44bccc293f99f1f315d769ef78
Dostified <146396287+Dostified@users.noreply.github.com>
2026-08-13 15:57:52 +0530

    Update config_flow.py

--- custom_components/ms_ip_cam/config_flow.py
+++ custom_components/ms_ip_cam/config_flow.py
@@ -4,7 +4,7 @@ from homeassistant import config_entries
 DOMAIN = "ms_ip_cam"
 
 class MSIPCamConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for MS IP Cam."""
+    """Handle config flow for MS IP Cam."""
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
@@ -12,8 +12,10 @@ class MSIPCamConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(title="MS IP Cam", data=user_input)
 
         data_schema = vol.Schema({
-            vol.Required("ip_address", default="192.168.1.100"): str,
-            vol.Required("rtsp_port", default=8554): int,
+            vol.Required(
+                "rtsp_url", 
+                default="rtsp://admin:admin123@192.168.1.100:8554/live"
+            ): str,
             vol.Required("http_port", default=8080): int,
         })
 
