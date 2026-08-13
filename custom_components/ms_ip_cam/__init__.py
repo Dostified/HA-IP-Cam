9334e27a8f1e34bffba6a73264cfd487fa39eb48
Dostified <146396287+Dostified@users.noreply.github.com>
2026-08-13 14:47:52 +0530

    Update __init__.py

--- custom_components/ms_ip_cam/__init__.py
+++ custom_components/ms_ip_cam/__init__.py
@@ -1,8 +1,9 @@
 from homeassistant.core import HomeAssistant
 
-DOMAIN = "my_pro_cam"
+DOMAIN = "ms_ip_cam"
 
 def setup(hass: HomeAssistant, config: dict) -> bool:
     hass.helpers.discovery.load_platform("camera", DOMAIN, {}, config)
     hass.helpers.discovery.load_platform("switch", DOMAIN, {}, config)
+    hass.helpers.discovery.load_platform("select", DOMAIN, {}, config)
     return True
