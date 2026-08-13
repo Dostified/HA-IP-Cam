903b97d139567501f01b3aaebaf6d3535ae05d05
Dostified <146396287+Dostified@users.noreply.github.com>
2026-08-13 15:50:48 +0530

    Update __init__.py

--- custom_components/ms_ip_cam/__init__.py
+++ custom_components/ms_ip_cam/__init__.py
@@ -2,13 +2,12 @@ from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 
 DOMAIN = "ms_ip_cam"
-PLATFORMS = ["camera", "switch", "select"]
+PLATFORMS = ["camera", "switch", "button", "select"]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up MS IP Cam from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-    # Store the user's IP address and Port in HA's memory
-    hass.data[DOMAIN][entry.entry_id] = entry.data 
+    hass.data[DOMAIN][entry.entry_id] = entry.data
     
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
