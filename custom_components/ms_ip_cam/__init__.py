4522f80ebf3427bac7d83b8fad01f919dca34371
Dostified <146396287+Dostified@users.noreply.github.com>
2026-08-13 15:58:10 +0530

    Update __init__.py

--- custom_components/ms_ip_cam/__init__.py
+++ custom_components/ms_ip_cam/__init__.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 
@@ -7,7 +8,19 @@ PLATFORMS = ["camera", "switch", "button", "select"]
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up MS IP Cam from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
+    
+    rtsp_url = entry.data["rtsp_url"]
+    http_port = entry.data.get("http_port", 8080)
+    
+    # Automatically extract the IP Address from the RTSP URL
+    parsed_url = urlparse(rtsp_url)
+    ip_address = parsed_url.hostname or "192.168.1.100"
+
+    hass.data[DOMAIN][entry.entry_id] = {
+        "rtsp_url": rtsp_url,
+        "ip_address": ip_address,
+        "http_port": http_port
+    }
     
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
